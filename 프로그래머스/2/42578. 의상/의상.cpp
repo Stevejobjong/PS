b40016f51ae5973fcc36f1ff5615036284b38f39
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2023-11-01 20:24:09 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string,int> m;
+    for(int i=0;i<clothes.size();i++){
+        m[clothes[i][1]]++;
+    }    
+    for(auto a : m){
+        answer*=a.second +1;
+    }
+    answer--;
+    return answer;
+}
