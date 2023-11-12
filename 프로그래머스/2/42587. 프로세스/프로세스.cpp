3aedc3732d9ec9711fb168811b4ac1bc1e00d10c
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2023-11-12 11:45:53 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    deque<pair<int,int>> q;
+	for (int i = 0; i < priorities.size(); i++) {
+		q.push_back({ i,priorities[i] });
+	}
+	int order = 1;
+	while (true) {
+		pair<int, int> f = q.front();
+		q.pop_front();
+		bool isProcessing = true;
+		for (auto node : q)
+		{
+			if (node.second > f.second)
+			{
+				q.push_back(f);
+				isProcessing = false;
+                break;
+			}
+		}
+		if (isProcessing) {
+			if (f.first == location)
+				return order;
+			else
+				order++;
+		}
+
+	}
+    return order;
+}
