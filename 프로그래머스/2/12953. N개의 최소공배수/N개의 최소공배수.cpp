3666378219e0caea4351bdf790f0019d12a3113c
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2023-12-14 09:26:11 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int LCM(int num1, int num2) {
+	int tmp = -1;
+	while (tmp!=0) {
+		tmp = num1 % num2;
+		num1 = num2;
+		num2 = tmp;
+		if (tmp == 0)
+			return num1;
+	}
+}
+
+int GCD(int num1, int num2) {
+	return num1 * num2 / LCM(num1, num2);
+}
+
+int solution(vector<int> arr) {
+	int answer = 0;
+	answer = arr[0];
+	for (int i = 1; i < arr.size(); i++) {
+		answer = GCD(answer, arr[i]);
+	}
+	return answer;
+}
