f9b306be703ca97960edc141580698ba6d1a6e46
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2023-12-28 09:25:19 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
+    
 	for (int i = 0; i < progresses.size(); i++) {
 		int cnt = 1;
-		bool release = true;
 		int day = (int)ceil((float)(100 - progresses[i]) / speeds[i]);
+		bool release = true;
+        
 		for (int j = i + 1; j < progresses.size(); j++) {
 			progresses[j] += day * speeds[j];
+            
 			if (progresses[j] >= 100 && release) {
 				i++;
 				cnt++;
@@ -16,7 +19,9 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
 			else
 				release = false;
 		}
+        
 		answer.push_back(cnt);
 	}
+    
     return answer;
 }
