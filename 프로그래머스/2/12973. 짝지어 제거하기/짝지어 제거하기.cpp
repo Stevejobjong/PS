0a13fbf8bdd3d1c05d258f88ad36087b780eddf5
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2024-05-08 21:13:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.68 ms, Memory: 6.46 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+	stack<char> stk;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (!stk.empty())
+		{
+			if (stk.top() == s[i])
+				stk.pop();
+			else
+				stk.push(s[i]);
+		}
+		else
+		{
+			stk.push(s[i]);
+		}
+	}
+	if (stk.empty())
+		answer = 1;
+	else
+		answer = 0;
+
+    return answer;
+}
