bf95eb3c69ec223cb6429b4936bee92cc745a39f
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2024-05-13 21:42:12 +0900

    [Silver I] Title: 후보 추천하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
@@ -14,21 +14,24 @@ int main(void)
 	{
 		int num;
 		cin >> num;
-		if (v.size() == N)
+		bool isFind = false;
+		int mn = 1001;
+		for (int i = 0; i < v.size(); i++)
 		{
-			bool isFind = false;
-			int mn = 1001;
-			for (int i = 0; i < v.size(); i++)
-			{
+			if (v.size() == N)
 				mn = min(mn, v[i].second);
-				if (num == v[i].first)
-				{
-					v[i].second++;
-					isFind = true;
-				}
-			}
-			if (!isFind)
+
+			if (num == v[i].first)
 			{
+				v[i].second++;
+				isFind = true;
+			}
+		}
+		if (!isFind)
+		{
+			if (v.size() == N)
+			{
+
 				for (int i = 0; i < v.size(); i++)
 				{
 					if (v[i].second == mn)
@@ -37,26 +40,10 @@ int main(void)
 						break;
 					}
 				}
-				v.push_back({ num,0 });
 			}
+			v.push_back({ num,0 });
 		}
-		else
-		{
-			bool isFind = false;
-			for (int i = 0; i < v.size(); i++)
-			{
-				if (num == v[i].first)
-				{
-					v[i].second += 1;
-					isFind = true;
-				}
-			}
-			if (!isFind)
-			{
-				v.push_back({ num,0 });
-			}
 
-		}
 	}
 	sort(v.begin(), v.end());
 	for (auto i : v)
